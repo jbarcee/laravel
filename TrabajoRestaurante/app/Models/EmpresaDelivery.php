367fe64c50deaac7b3435a992fd88e8bfc057227
Jorge Bárcena <barcenaferruz2022@iessantiagohernandez.com>
2025-01-30 13:24:25 +0100

    Trabajo Restaurante empezado

---
+++ TrabajoRestaurante/app/Models/EmpresaDelivery.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmpresaDelivery extends Model
+{
+    private $id;
+    private $nombre;
+    private $precio;
+}
---
+++ TrabajoRestaurante/app/Models/Plato_Pedido.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plato_Pedido extends Model
+{
+    private $idPlato;
+    private $idPedido;
+}
